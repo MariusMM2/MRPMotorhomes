14317ee79cc1fc393f80750c4e5c8b471cb7e278
MariusMM2 <dmunteanu91@yahoo.com>
2018-04-24 11:23:05 +0200

    initial commit

---
+++ src/main/java/com/mrp/motorhomes/MotorhomesApplication.java
@@ -0,0 +1,12 @@
+package com.mrp.motorhomes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorhomesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotorhomesApplication.class, args);
+	}
+}
