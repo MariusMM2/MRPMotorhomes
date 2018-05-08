11dd27f092a673785f27e520234a846eaa2cbb90
Cons19 <razvan1598@gmail.com>
2018-05-08 08:53:35 +0200

    commit test

--- src/main/java/com/mrp/motorhomes/MotorhomesApplication.java
+++ src/main/java/com/mrp/motorhomes/MotorhomesApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MotorhomesApplication {
 
 	public static void main(String[] args) {
-		System.out.println("marius");
+        System.out.println("razvan");
+
 
 		SpringApplication.run(MotorhomesApplication.class, args);
 	}
