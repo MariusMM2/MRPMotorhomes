b700727446f017413f551f02073bb1351f6a45b8
MariusMM2 <dmunteanu91@yahoo.com>
2018-05-08 08:54:44 +0200

    test2

--- src/main/java/com/mrp/motorhomes/MotorhomesApplication.java
+++ src/main/java/com/mrp/motorhomes/MotorhomesApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MotorhomesApplication {
 
 	public static void main(String[] args) {
+		
 		System.out.println("marius");
 
 		SpringApplication.run(MotorhomesApplication.class, args);
