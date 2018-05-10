bcc5e5491d562cfe9acbe14b7c922d91100cdfd5
Cons19 <razvan1598@gmail.com>
2018-05-10 16:47:45 +0200

    commit test

--- src/main/java/com/mrp/motorhomes/repositories/MotorhomeRepository.java
+++ src/main/java/com/mrp/motorhomes/repositories/MotorhomeRepository.java
@@ -2,6 +2,8 @@ package com.mrp.motorhomes.repositories;
 
 import com.mrp.motorhomes.model.Motorhome;
 
+import java.sql.Date;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class MotorhomeRepository extends CrudRepository<Motorhome> {
@@ -13,12 +15,49 @@ public class MotorhomeRepository extends CrudRepository<Motorhome> {
 	@Override
 	public void create(Motorhome item) {
 		// TODO - implement MotorhomeRepository.create
+
+		try {
+			preparedStatement = connection.prepareStatement("INSERT INTO motorhomes(brand, type, model, year, lastService, basePrice) VALUES (?,?,?,?,?,?)");
+			preparedStatement.setString(1, item.getBrand());
+			preparedStatement.setString(2, item.getType());
+			preparedStatement.setString(3, item.getModel());
+			preparedStatement.setString(4, item.getYear());
+			preparedStatement.setDate(5,Date.valueOf(item.getLastService()));
+			preparedStatement.setDouble(6,item.getBasePrice());
+
+			preparedStatement.execute();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+
 		throw new UnsupportedOperationException();
 	}
 	
 	@Override
 	public ArrayList<Motorhome> readAll() {
 		// TODO - implement MotorhomeRepository.readAll
+
+		ArrayList<Motorhome> motorhomes = new ArrayList<>();
+
+		try {
+			preparedStatement = connection.prepareStatement("SELECT * FROM motorhomes");
+			resultSet = preparedStatement.executeQuery();
+			while(resultSet.next())
+			{
+				motorhomes.add(new Motorhome(resultSet.getInt("id"),
+						resultSet.getString("brand"),
+						resultSet.getString("type"),
+						resultSet.getString("model"),
+						resultSet.getString("year"),
+						resultSet.getDate("lastService").toLocalDate(),
+						resultSet.getDouble("basePrice")));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
 		throw new UnsupportedOperationException();
 	}
 
@@ -29,6 +68,25 @@ public class MotorhomeRepository extends CrudRepository<Motorhome> {
 	@Override
 	public Motorhome read(int id) {
 		// TODO - implement MotorhomeRepository.read
+
+		try {
+			preparedStatement = connection.prepareStatement("SELECT * FROM motorhomes WHERE motorhomes.id = id");
+			resultSet = preparedStatement.executeQuery();
+
+			if(resultSet.next()) {
+				return new Motorhome(resultSet.getInt("id"),
+						resultSet.getString("brand"),
+						resultSet.getString("type"),
+						resultSet.getString("model"),
+						resultSet.getString("year"),
+						resultSet.getDate("lastService").toLocalDate(),
+						resultSet.getDouble("basePrice"));
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
 		throw new UnsupportedOperationException();
 	}
 
