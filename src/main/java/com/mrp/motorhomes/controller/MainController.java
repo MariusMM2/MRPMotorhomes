f2e5643c780fc47b4854aead1075b8090f771bbd
MariusMM2 <dmunteanu91@yahoo.com>
2018-05-11 19:16:37 +0200

    *cough* removed generic template from maincontroller

--- src/main/java/com/mrp/motorhomes/controller/MainController.java
+++ src/main/java/com/mrp/motorhomes/controller/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
 
 @Controller
-public class MainController<T> {
+public class MainController {
 	/**
 	 * 
 	 * @param model
