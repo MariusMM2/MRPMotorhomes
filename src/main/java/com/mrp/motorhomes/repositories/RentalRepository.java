c2aed22c9cee8cecbe2328ac494675913f14bdc4
panaitescu-paul <panaitescupaul@yahoo.com>
2018-05-11 23:53:23 +0200

    SQL Table - rentals Model - Rental - change 2 variables, setters, and getters to CamelCase Repository - Rental - create, readAll, read methods

--- src/main/java/com/mrp/motorhomes/repositories/RentalRepository.java
+++ src/main/java/com/mrp/motorhomes/repositories/RentalRepository.java
@@ -1,7 +1,10 @@
 package com.mrp.motorhomes.repositories;
 
+import com.mrp.motorhomes.model.Motorhome;
 import com.mrp.motorhomes.model.Rental;
 
+import java.sql.Date;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class RentalRepository extends CrudRepository<Rental> {
@@ -12,13 +15,52 @@ public class RentalRepository extends CrudRepository<Rental> {
 	 */
 	@Override
 	public void create(Rental item) {
-		// TODO - implement RentalRepository.create
-		throw new UnsupportedOperationException();
+
+		try {
+			preparedStatement = connection.prepareStatement("INSERT INTO rentals(customerId, motorhomeId, price, startDate, endDate, pickUp, dropOff) VALUES (?, ?, ?, ?, ?, ?, ?)");
+			preparedStatement.setInt(1, item.getCustomerId());
+			preparedStatement.setInt(2, item.getMotorhomeId());
+			preparedStatement.setDouble(3, item.getPrice());
+			preparedStatement.setDate(4, Date.valueOf(item.getStartDate()));
+			preparedStatement.setDate(5, Date.valueOf(item.getEndDate()));
+			preparedStatement.setString(6, item.getPickUp());
+			preparedStatement.setString(7, item.getDropOff());
+
+			preparedStatement.execute();
+
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+//		throw new UnsupportedOperationException();
 	}
 	
 	@Override
 	public ArrayList<Rental> readAll() {
-		// TODO - implement RentalRepository.readAll
+
+		ArrayList<Rental> rentals = new ArrayList<>();
+
+		try {
+			preparedStatement = connection.prepareStatement("SELECT * FROM rentals");
+			resultSet = preparedStatement.executeQuery();
+			while(resultSet.next())
+			{
+				rentals.add(new Rental(
+						resultSet.getInt("id"),
+						resultSet.getInt("customerId"),
+						resultSet.getInt("motorhomeId"),
+						resultSet.getDouble("price"),
+						resultSet.getDate("startDate").toLocalDate(),
+						resultSet.getDate("endDate").toLocalDate(),
+						resultSet.getString("pickUp"),
+						resultSet.getString("dropOff")));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+
 		throw new UnsupportedOperationException();
 	}
 
@@ -28,8 +70,30 @@ public class RentalRepository extends CrudRepository<Rental> {
 	 */
 	@Override
 	public Rental read(int id) {
-		// TODO - implement RentalRepository.read
-		throw new UnsupportedOperationException();
+
+		try {
+			preparedStatement = connection.prepareStatement("SELECT * FROM motorhomes WHERE id = ?");
+			preparedStatement.setInt(1, id);
+			resultSet = preparedStatement.executeQuery();
+
+			if(resultSet.next()) {
+				return new Rental(
+						resultSet.getInt("id"),
+						resultSet.getInt("customerId"),
+						resultSet.getInt("motorhomeId"),
+						resultSet.getDouble("price"),
+						resultSet.getDate("startDate").toLocalDate(),
+						resultSet.getDate("endDate").toLocalDate(),
+						resultSet.getString("pickUp"),
+						resultSet.getString("dropOff"));
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+
+		//		throw new UnsupportedOperationException();
 	}
 
 	/**
