a68ac6ec3869bcaff84820e944101a1e47894d92
MariusMM2 <dmunteanu91@yahoo.com>
2018-05-11 20:28:29 +0200

    updated customer controller

--- src/main/java/com/mrp/motorhomes/controller/CustomerController.java
+++ src/main/java/com/mrp/motorhomes/controller/CustomerController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CustomerController {
@@ -32,5 +33,11 @@ public class CustomerController {
 	public String create(@ModelAttribute Customer customer){
 		repository.create(customer);
 		return "redirect:/customers";
+	}
+	
+	@GetMapping("/customers/details")
+	public String details(@RequestParam("id") int id, Model model){
+		model.addAttribute(repository.read(id));
+		return "customers/details";
 	}
 }
